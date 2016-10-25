e817d92673e7bb70aaeca4b45b78e365b686898e
Mayra Garcia Cortes <alu0100831818@ull.edu.es>
2016-10-25 23:03:14 +0000

    Menu de prueba de cara al programador

--- main.rb
+++ main.rb
@@ -1,3 +1,14 @@
+require './lib/receta.rb'
 
-
-require 'lib/receta.rb'
+#principal de prueba
+#==============================================================
+d = Receta.new()  
+puts "#{d.mostrar_cabecera('pollo')}"
+puts "#{d.mostrar_porciones_t_espera(2,3)}"
+puts "#{d.ingredientes('2 rebanadas de pan, 1 loncha de jamon, 1 loncha de queso, mantequilla al gusto')}"
+puts "#{d.instrucciones('Ponga hervir el agua.
+,Añada la pasta
+,deje hervir durante 6 minutos. 
+,Escurra el agua.
+,Mezcle la salsa de tomate y la pimienta con la pasta.
+,Sirva caliente.')}"
