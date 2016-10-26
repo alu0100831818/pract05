1618f7f7397fea34b035f694b534d7edc34a4769
Mayra Garcia Cortes <alu0100831818@ull.edu.es>
2016-10-26 10:15:52 +0000

    mejora del rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,31 @@
+#Mayra Garcia, LPP
+#Rakefile, clase: Receta
 
-task :test do
-  ruby "test/Test_Receta.rb"
+#Probar test cabecera
+task :cabecera do
+  sh "ruby -I. test/Test_Receta.rb -n test_Ingredientes"
 end
 
-task :default => [:test]
+#Probar test porciones y tiempo
+task :Porciones_tiempo do
+  sh "ruby -I. test/Test_Receta.rb -n test_Porciones_tiempo"
+end
+
+#Probar test ingredientes
+task :Ingredientes do
+  sh "ruby -I. test/Test_Receta.rb -n test_Ingredientes"
+end
+
+#Probar test instrucciones
+task :instrucciones do
+  sh "ruby -I. test/Test_Receta.rb -n test_instrucciones"
+end
+
+#Probar todos los test
+task :all do
+  sh "ruby -I. test/Test_Receta.rb"
+end
+
+
+task :default => [:cabecera, :Porciones_tiempo]
+task :default => [:all]
